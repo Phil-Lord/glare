451cb7c9d22938e85bede927373059fd296d1d89
Phil Lord <phillord2010@gmail.com>
2024-10-14 21:25:19 +0100

    Set index whitespace to tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,23 @@
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Glare</Text>
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Glare</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f8f8ff',
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
-    },
-  });
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#f8f8ff',
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		marginBottom: 20,
+	},
+});
