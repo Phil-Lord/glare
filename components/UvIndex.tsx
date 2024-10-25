8d2a87ab66bc66404fa41bed7389b6eb28058e92
Phil Lord <phillord2010@gmail.com>
2024-10-26 00:09:20 +0100

    Add Index type

--- components/UvIndex.tsx
+++ components/UvIndex.tsx
@@ -1,5 +1,11 @@
 import { ThemedText } from "./ThemedText";
 
+type Index = {
+	id: string,
+	time: string;
+	uvIndex: number;
+};
+
 export default function UvIndex() {
 	return <ThemedText type='defaultSemiBold'>0</ThemedText>
 }
