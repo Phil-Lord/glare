5fe3a33371ab59ef38988bd7f35be86cf95538ea
Phil Lord <phillord2010@gmail.com>
2024-10-26 00:10:52 +0100

    Add fetch params, url, and date range function

--- components/UvIndex.tsx
+++ components/UvIndex.tsx
@@ -7,5 +7,17 @@ type Index = {
 };
 
 export default function UvIndex() {
-	return <ThemedText type='defaultSemiBold'>0</ThemedText>
-}
+	const params = {
+		"latitude": 53.9908,
+		"longitude": -1.5373,
+		"hourly": "uv_index",
+		"timezone": "Europe/London",
+		"past_hours": 6,
+		"forecast_days": 1,
+		"forecast_hours": 6
+	};
+	const url = "https://api.open-meteo.com/v1/forecast";
+
+	const range = (start: number, stop: number, step: number) =>
+		Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
