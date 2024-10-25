eefa5b1c7ef9df680860d0412e97c9372cc073bd
Phil Lord <phillord2010@gmail.com>
2024-10-26 00:14:02 +0100

    Add uv index fetch with state data

--- components/UvIndex.tsx
+++ components/UvIndex.tsx
@@ -1,3 +1,8 @@
+import { useEffect, useState } from 'react';
+
+import { fetchWeatherApi } from 'openmeteo';
+import 'text-encoding';
+
 import { ThemedText } from "./ThemedText";
 
 type Index = {
@@ -7,6 +12,9 @@ type Index = {
 };
 
 export default function UvIndex() {
+	const [isLoading, setLoading] = useState(true);
+	const [data, setData] = useState<Index[]>([]);
+
 	const params = {
 		"latitude": 53.9908,
 		"longitude": -1.5373,
@@ -20,4 +28,38 @@ export default function UvIndex() {
 
 	const range = (start: number, stop: number, step: number) =>
 		Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+	const getUvIndex = async () => {
+		try {
+			const responses = await fetchWeatherApi(url, params);
+			const response = responses[0];
+
+			const utcOffsetSeconds = response.utcOffsetSeconds();
+			const hourly = response.hourly()!;
+			const weatherData = {
+				hourly: {
+					time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
+						(t) => new Date((t + utcOffsetSeconds) * 1000)
+					),
+					uvIndex: hourly.variables(0)!.valuesArray()!,
+				},
+			};
+
+			const timeToIndex = weatherData.hourly.time.map((date, i) => ({
+				id: String(i),
+				time: date.toISOString(),
+				uvIndex: weatherData.hourly.uvIndex[i]
+			}))
+
+			setData(timeToIndex);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getUvIndex();
+	}, []);
 
