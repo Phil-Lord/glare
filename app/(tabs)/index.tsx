b35899653d463000c6d388d32da221099e6865e7
Phil Lord <phillord2010@gmail.com>
2024-10-30 23:26:36 +0000

    Move UvIndex component into subfolder to fix repo naming bug

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/theme/ThemedText';
 import { ThemedView } from '@/components/theme/ThemedView';
-import UvIndex from '@/components/UvIndex';
+import UvIndex from '@/components/uvIndex/UvIndex';
 
 export default function HomeScreen() {
 	return (
