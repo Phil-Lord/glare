9a54ff2f0e5a5c934fcfbb3bd6995dc05306d63f
Phil Lord <phillord2010@gmail.com>
2024-10-30 22:25:26 +0000

    Compose UvData in UvIndex component

--- components/UvIndex.tsx
+++ components/UvIndex.tsx
@@ -1,73 +1,23 @@
 import { ActivityIndicator, FlatList, StyleSheet } from 'react-native';
 import { useEffect, useState } from 'react';
 
-import { fetchWeatherApi } from 'openmeteo';
 import 'text-encoding';
 
+import fetchUvData from '@/services/uvService';
+import formatUvData from '@/utils/formatUvData';
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from './ThemedView';
-
-type Index = {
-	id: string,
-	time: string;
-	uvIndex: string;
-};
+import { UvData } from '@/types/uv';
 
 const UvIndex = () => {
 	const [isLoading, setLoading] = useState(true);
-	const [data, setData] = useState<Index[]>([]);
-
-	const params = {
-		"latitude": 53.9908,
-		"longitude": -1.5373,
-		"hourly": "uv_index",
-		"timezone": "Europe/London",
-		"past_hours": 6,
-		"forecast_days": 1,
-		"forecast_hours": 6
-	};
-	const url = "https://api.open-meteo.com/v1/forecast";
-
-	// Helper function to form time ranges.
-	// E.g. start and stop can be UNIX timestamps, with interval being 3600 (an hour).
-	const timeRange = (start: number, stop: number, step: number) =>
-		Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+	const [data, setData] = useState<UvData[]>([]);
 
 	const getHourlyIndicies = async () => {
 		try {
-			const responses = await fetchWeatherApi(url, params);
-			const response = responses[0];
-
-			const utcOffsetSeconds = response.utcOffsetSeconds();
-
-			// Extract and decode UNIX timestamps and hourly interval (3600)
-			const weatherDataBytes = response.hourly()!;
-			const startTime = Number(weatherDataBytes.time());
-			const endTime = Number(weatherDataBytes.timeEnd());
-			const interval = weatherDataBytes.interval();
-
-			// Create time and UV index arrays
-			const weatherData = {
-				times: timeRange(startTime, endTime, interval).map(
-					(t) => new Date((t + utcOffsetSeconds) * 1000)
-				),
-				uvIndicies: weatherDataBytes.variables(0)!.valuesArray()!
-			};
-
-			const options: Intl.DateTimeFormatOptions = {
-				year: '2-digit',
-				month: '2-digit',
-				day: '2-digit',
-				hour: '2-digit',
-				minute: '2-digit'
-			};
-			const hourlyIndicies = weatherData.times.map((date, i) => ({
-				id: String(i),
-				time: date.toLocaleString(undefined, options),
-				uvIndex: weatherData.uvIndicies[i].toFixed(2)
-			}))
-
-			setData(hourlyIndicies);
+			const data = await fetchUvData();
+			const formattedData = formatUvData(data);
+			setData(formattedData);
 		} catch (error) {
 			console.log('Error fetching UV index:', error);
 		} finally {
@@ -85,7 +35,6 @@ const UvIndex = () => {
 		) : (
 			<FlatList
 				data={data}
-				keyExtractor={({ id }) => id}
 				renderItem={({ item }) => (
 					<ThemedText>{item.time}: {item.uvIndex}</ThemedText>
 				)}
